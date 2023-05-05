1a66a8b60dc6670b89f32e4f0787a4b93855ac8c
Aditya Raman <114894627+theadityaraman@users.noreply.github.com>
2023-05-05 02:09:31 -0400

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,1 +1,47 @@
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  datesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  dateButton: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+  },
+  selectedDateButton: {
+    backgroundColor: '#00bcd4',
+    borderColor: '#00bcd4',
+  },
+  dateButtonText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  selectedDateButtonText: {
+    color: '#fff',
+  },
+  submitButton: {
+    backgroundColor: '#00bcd4',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  submitButtonText: {
+    fontSize: 16,
+    color: '#fff',
+  },
